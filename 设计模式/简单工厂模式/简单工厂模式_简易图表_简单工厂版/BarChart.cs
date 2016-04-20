bcb8de5e2382bea6f7a90de972e6f4c78aa2e628
ningboliuwei <liuwei@nbu.edu.cn>
2016-04-20 14:47:07 +0800

    simple factory - chart bug fix

--- 设计模式/简单工厂模式/简单工厂模式_简易图表_简单工厂版/BarChart.cs
+++ 设计模式/简单工厂模式/简单工厂模式_简易图表_简单工厂版/BarChart.cs
@@ -19,9 +19,9 @@ namespace 简单工厂模式_简易图表_简单工厂版
 			_g.DrawLine(_p, 100, 600, 600, 600);
 			_g.DrawLine(_p, 100, 100, 100, 600);
 
-			for (int i = 0; i < Data.Length - 1; i++)
+			for (int i = 0; i <= Data.Length - 1; i++)
 			{
-				_g.DrawRectangle(_p, 150 + i * 100, 600 - Data[i], 50, Data[i]);
+				_g.DrawRectangle(_p, 150 + i * 80, 600 - Data[i], 50, Data[i]);
 			}
 		}
 	}
--- 设计模式/简单工厂模式/简单工厂模式_简易图表_简单工厂版/LineChart.cs
+++ 设计模式/简单工厂模式/简单工厂模式_简易图表_简单工厂版/LineChart.cs
@@ -19,9 +19,9 @@ namespace 简单工厂模式_简易图表_简单工厂版
 			_g.DrawLine(_p, 100, 600, 600, 600);
 			_g.DrawLine(_p, 100, 100, 100, 600);
 
-			for (int i = 0; i < Data.Length - 1; i++)
+			for (int i = 1; i <= Data.Length - 1; i++)
 			{
-				_g.DrawLine(_p, 150 + i * 100, 600 - Data[i], 150 + (i + 1) * 100, 600 - Data[i + 1]);
+				_g.DrawLine(_p, i * 100, 600 - Data[i-1], (i + 1) * 100, 600 - Data[i]);
 			}
 		}
 	}
