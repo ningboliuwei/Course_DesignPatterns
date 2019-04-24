6c811299d89f5acb6a97476366ed9995024baad9
ningboliuwei <liuwei@nbu.edu.cn>
2019-04-24 14:25:33 +0800

    增加了 context 类

---
+++ 实验/实验09_加密策略模式/CipherContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 实验09_加密策略模式
+{
+    class CipherContext
+    {
+        private ICipher _cipher;
+
+        public CipherContext(ICipher cipher)
+        {
+            _cipher = cipher;
+        }
+
+        public string Encrypt(string text)
+        {
+            return _cipher.GetEncryptedText(text);
+        }
+    }
+}
--- 实验/实验09_加密策略模式/Program.cs
+++ 实验/实验09_加密策略模式/Program.cs
@@ -10,13 +10,27 @@ namespace 实验09_加密策略模式
     {
         static void Main(string[] args)
         {
-            var ciphers = new List<ICipher> {new MD5Cipher(), new SHACipher(), new RSACipher()};
-
             Console.Write("请输入要加密的字符串：");
             var text = Console.ReadLine();
             Console.Write("请选择加密算法：1. MD5 2. SHA 3. RSA\n");
             var option = Convert.ToInt32(Console.ReadLine()) - 1;
-            Console.WriteLine($"加密后的字符串为：{ciphers[option].GetEncryptedText(text)}");
+
+            CipherContext context = null;
+
+            if (option == 1)
+            {
+                context = new CipherContext(new MD5Cipher());
+            }
+            else if (option == 2)
+            {
+                context = new CipherContext(new SHACipher());
+            }
+            else if (option == 2)
+            {
+                context = new CipherContext(new RSACipher());
+            }
+
+            Console.WriteLine($"加密后的字符串为：{context?.Encrypt(text)}");
 
             Console.ReadKey();
         }
